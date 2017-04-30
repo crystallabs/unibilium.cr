996af1e81a3de42be87906c0a91573acd963e3e4
Benoit de Chezelles <lesell_b@epitech.eu>
2017-04-30 19:36:22 +0200

    Add basic init/dummy + get/set for str,num,bool,aliases

--- spec/unibilium_spec.cr
+++ spec/unibilium_spec.cr
@@ -1,9 +1,63 @@
 require "./spec_helper"
 
-describe Unibilium do
-  # TODO: Write tests
+describe Unibilium::Terminfo do
+  it "can be destroyed" do
+    t = Unibilium::Terminfo.dummy
+    t.destroyed?.should be_false
 
-  it "works" do
-    false.should eq(true)
+    t.destroy
+    t.destroyed?.should be_true
+  end
+
+  it "set & get terminal name" do
+    Unibilium::Terminfo.with_dummy do |t|
+      t.term_name.should eq "unibilium dummy terminal"
+
+      t.term_name = "my terminal"
+      t.term_name.should eq "my terminal"
+    end
+  end
+
+  it "set & get bool id" do
+    Unibilium::Terminfo.with_dummy do |t|
+      id = Unibilium::Entry::Boolean::Has_meta_key
+
+      t.set_bool(id, true)
+      t.get_bool(id).should be_true
+
+      t.set_bool(id, false)
+      t.get_bool(id).should be_false
+    end
+  end
+
+  it "set & get numeric id" do
+    Unibilium::Terminfo.with_dummy do |t|
+      id = Unibilium::Entry::Numeric::Lines
+
+      t.set_num(id, 0)
+      t.get_num(id).should eq 0
+
+      t.set_num(id, 42)
+      t.get_num(id).should eq 42
+    end
+  end
+
+  it "set & get string id" do
+    Unibilium::Terminfo.with_dummy do |t|
+      id = Unibilium::Entry::String::Carriage_return
+
+      t.set_str(id, "")
+      t.get_str(id).should eq ""
+
+      t.set_str(id, "\r\n")
+      t.get_str(id).should eq "\r\n"
+    end
+  end
+
+  it "set & get aliases" do
+    Unibilium::Terminfo.with_dummy do |t|
+      t.aliases = ["abc", "def"]
+      t.aliases.should eq ["abc", "def"]
+    end
   end
 end
--- src/unibilium.cr
+++ src/unibilium.cr
@@ -1,5 +1,90 @@
-require "./unibilium/*"
+require "./lib_unibilium"
 
 module Unibilium
-  # TODO Put your code here
+  class Terminfo
+    @term : LibUnibilium::Terminfo
+    @save_aliases : Array(LibC::Char*)?
+
+    def self.dummy
+      new(LibUnibilium.dummy)
+    end
+
+    def self.with_dummy
+      terminfo = dummy
+      begin
+        yield terminfo
+      ensure
+        terminfo.destroy
+      end
+    end
+
+    def initialize(@term)
+    end
+
+    def destroyed?
+      return @term.null?
+    end
+
+    def destroy
+      return if @term.null?
+
+      LibUnibilium.destroy(self)
+      @term = LibUnibilium::Terminfo.null
+    end
+
+    def to_unsafe
+      @term
+    end
+
+    def term_name
+      String.new LibUnibilium.get_name(self)
+    end
+
+    def term_name=(name)
+      LibUnibilium.set_name(self, name)
+    end
+
+    # TODO: refactor
+    def get_bool(id)
+      LibUnibilium.get_bool(self, id)
+    end
+
+    def set_bool(id, value)
+      LibUnibilium.set_bool(self, id, value)
+    end
+
+    def get_str(id)
+      String.new LibUnibilium.get_str(self, id)
+    end
+
+    def set_str(id, value)
+      LibUnibilium.set_str(self, id, value)
+    end
+
+    def get_num(id)
+      LibUnibilium.get_num(self, id)
+    end
+
+    def set_num(id, value)
+      LibUnibilium.set_num(self, id, value)
+    end
+    # end TODO refactor
+
+    def aliases
+      ptr = LibUnibilium.get_aliases(self)
+
+      ary = [] of String
+      until ptr.value.null?
+        ary << String.new(ptr.value)
+        ptr += 1
+      end
+
+      ary
+    end
+
+    def aliases=(aliases : Array(String))
+      @save_aliases = aliases.map(&.to_unsafe)
+      LibUnibilium.set_aliases(self, @save_aliases.not_nil!)
+    end
+  end
 end
